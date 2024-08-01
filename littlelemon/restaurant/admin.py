7e391f5397579eec5dc29db84bde44320fb221fa
n3pite00 <n3pite00@students.oamk.fi>
2024-08-01 17:51:05 +0300

    Update.

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -3,10 +3,5 @@ from .models import MenuItem, Booking
 
 # Register your models here.
 
-@admin.register(MenuItem)
-class MenuAdmin(admin.ModelAdmin):
-    list_display = (id, 'Title', 'Price', 'Inventory')
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = (id, 'Name', 'No_of_guests', 'BookingDate')
+admin.site.register(MenuItem)
+admin.site.register(Booking)
