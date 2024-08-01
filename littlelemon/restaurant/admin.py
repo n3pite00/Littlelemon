d15a7e85e19997618bf2c1124ee27e8399f899bb
n3pite00 <n3pite00@students.oamk.fi>
2024-08-01 13:30:28 +0300

    Models and mysql added to the project.

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (id, 'Name', 'No_of_guests', 'BookingDate')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (id, 'Title', 'Price', 'Inventory')
